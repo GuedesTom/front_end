6c1d6bb6c85e09766b8869f8ec3b05aa2e851435
Guedes Tom <92975112+GuedesTom@users.noreply.github.com>
2022-04-30 14:38:35 +0200

    Add Modif user

---
+++ src/Components/Administration/User.jsx
@@ -0,0 +1,36 @@
+import "../../App.css";
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { SagagaContext } from "../../SagagaContext";
+
+export default function AdminUser() {
+  let { token } = useContext(SagagaContext);
+  const [listAAfficher, setlistAAfficher] = useState([]);
+  let navigate = useNavigate();
+
+  const updateContent = (id) => {
+    navigate(`/Update/${id}`);
+  };
+
+  useEffect(() => {
+    axios
+      .get("/api/content")
+      .then((res) => setlistAAfficher(res.data))
+      .catch((err) => console.log(err.response));
+  }, [token]);
+
+  return (
+    <div id="container">
+      {listAAfficher.map((content) => {
+        console.log("first");
+        return (
+          <div id="card" key={content._id}>
+            <li id="title">{content.name}</li>
+            <button onClick={() => updateContent(content._id)}>Modifier</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
