964a183f9927731908b900d2f30478aa884d777c
Guedes Tom <92975112+GuedesTom@users.noreply.github.com>
2022-05-07 10:42:30 +0200

    adding card

--- src/Components/Accueil.jsx
+++ src/Components/Accueil.jsx
@@ -63,52 +63,65 @@ export default function Accueil() {
   return (
     <div id="container">
       {listAAfficher.map((content) => {
+        // mon affichage devrait s'appliquer simplement au chargement de la page... malheureusement il boucle a l'infini...
         console.log("first");
         return (
-          <div id="card" key={content._id}>
-            <li id="title">
-              {content.name}
-              <ul>
-                <img
-                  src={`/api/picture/download/${content.picture}`}
-                  alt=""
-                  height="10px"
-                  lengt="10px"
-                />
-              </ul>
-              <p>{content.description} </p>
-            </li>
+          <div class="container">
+	<div class="card">
+		<figure class="card__thumb">
+			<img src="https://source.unsplash.com/75S9fpDJVdo/300x510" alt="Picture by Kyle Cottrell" class="card__image">
+			<figcaption class="card__caption">
+				<h2 class="card__title">NASA Has Found Hundreds Of Potential New Planets</h2>
+				<p class="card__snippet">NASA released a list of 219 new “planet candidates” discovered by the Kepler space telescope, 10 of which are similar to Earth’s size and may be habitable by other life forms.</p>
+				<a href="" class="card__button">Read more</a>
+			</figcaption>
+		</figure>
+	</div>
+      //     <div id="card" key={content._id}>
+      //       <li id="title">
+      //         {content.name}
+      //         <ul>
+      //           <img
+      //             src={`/api/picture/download/${content.picture}`}
+      //             alt=""
+      //             height="80px"
+      //             lengt="80px"
+      //           />
+      //         </ul>
+      //         <p>{content.description} </p>
+      //       </li>
 
-            <button
-              onClick={() => details(content._id)}
-              class="custom-btn btn-6"
-            >
-              Details
-            </button>
-            {token ? (
-              added.forEach((element) => {
-                element._id === content._id ? (
-                  <button class="custom-btn btn-6">
-                    <span>Deja Ajouter</span>
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => song(content._id)}
-                    class="custom-btn btn-6"
-                  ><span>Ecouter plus tard</span></button>
-                );
-              })
-            ) : (
-              <button
-                onClick={() => navigate("/Connexion")}
-                class="custom-btn btn-6"
-              >
-                <span>Connexion</span>
-              </button>
-            )}
-          </div>
-        );
-      })}
-    </div>
+      //       <button
+      //         onClick={() => details(content._id)}
+      //         class="custom-btn btn-6"
+      //       >
+      //         Details
+      //       </button>
+      //       {token ? (
+      //         added.forEach((element) => {
+      //           element._id === content._id ? (
+      //             <button class="custom-btn btn-6">
+      //               <span>Deja Ajouter</span>
+      //             </button>
+      //           ) : (
+      //             <button
+      //               onClick={() => song(content._id)}
+      //               class="custom-btn btn-6"
+      //             >
+      //               <span>Ecouter plus tard</span>
+      //             </button>
+      //           );
+      //         })
+      //       ) : (
+      //         <button
+      //           onClick={() => navigate("/Connexion")}
+      //           class="custom-btn btn-6"
+      //         >
+      //           <span>Connexion</span>
+      //         </button>
+      //       )}
+      //     </div>
+      //   );
+      // })}
   );
 }
