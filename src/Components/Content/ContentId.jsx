6a96826cd65130e9b67de046ff2114ba7cc9bba0
GuedesTom <guedes.tomtom@outlook.fr>
2022-05-21 16:23:14 +0200

    Change design contentId

--- src/Components/Content/ContentId.jsx
+++ src/Components/Content/ContentId.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Modal, { useModalState } from "react-simple-modal-provider";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
@@ -13,17 +12,37 @@ export default function ContentId() {
       .catch((err) => console.log(err.response));
   });
   return (
-      <div>
-        <li>
-          {contentAAfficher.name}
-
-          <audio
-            src={`/api/file/download/${contentAAfficher.filename}`}
-            controls
+    <div>
+      <li>
+        <a class="card">
+          <img
+            src={`/api/picture/download/${contentAAfficher.picture}`}
+            class="card__image"
+            alt=""
           />
-
-          <p>{contentAAfficher.description}</p>
-        </li>
-      </div>
+          <div class="card__overlay">
+            <div class="card__header">
+              <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+                <path />
+              </svg>
+              <img
+                class="card__thumb"
+                src="https://cdn-icons.flaticon.com/png/512/1665/premium/1665680.png?token=exp=1651931861~hmac=e2fcf4966565dba5b9ca620054b6e629"
+                alt=""
+              />
+              <div class="card__header-text">
+                <h3 class="card__title">{contentAAfficher.name}</h3>
+                <span class="card__status">{contentAAfficher.genres}</span>
+              </div>
+            </div>
+            <audio
+              class="card__audio"
+              src={`/api/song/download/${contentAAfficher.song}`}
+              controls
+            />
+          </div>
+        </a>
+      </li>
+    </div>
   );
 }
